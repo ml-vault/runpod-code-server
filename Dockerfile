b363f5f35a7de9889ab450e7c8a39660badee398
Yoonsoo Kim <laptise@live.jp>
2024-05-01 12:21:26 +0000

    use vscode

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM runpod/pytorch:2.2.0-py3.10-cuda12.1.1-devel-ubuntu22.04
-RUN curl -fsSL https://code-server.dev/install.sh | sh
+RUN apt update
+RUN curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
+RUN api install -y ./vscode.deb
 COPY ./run-server.sh /run-server.sh
 CMD ["sh", "/run-server.sh"]
--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,3 @@
 mkdir -p /workspace/.vscode/extensions
 mkdir -p /workspace/.vscode/user
-code-server --auth none --bind-addr 0.0.0.0:25431 --extensions-dir /workspace/.vscode/extensions --user-data-dir /workspace/.vscode/user
+code serve-web --without-connection-token --host 0.0.0.0 --extensions-dir /workspace/.vscode/extensions --user-data-dir /workspace/.vscode/user
